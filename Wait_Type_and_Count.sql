5704ef0340150b3192e609cdb4bdddfe21264beb
Eleonora Grigoryan <eleonora.grigoryan@outlook.com>
2018-09-30 09:49:46 +0400

    Create Wait_Type_and_Count.sql

---
+++ Wait_Type_and_Count.sql
@@ -0,0 +1,12 @@
+-- TSQL
+
+-- Shows all wait types and their occurances in the server since the last server start
+
+SELECT wait_type ,
+SUM(wait_time_ms / 1000) AS [wait_time_s]
+FROM sys.dm_os_wait_stats DOWS
+WHERE wait_type NOT IN ( 'SLEEP_TASK', 'BROKER_TASK_STOP',
+'SQLTRACE_BUFFER_FLUSH', 'CLR_AUTO_EVENT',
+'CLR_MANUAL_EVENT', 'LAZYWRITER_SLEEP' )
+GROUP BY wait_type
+ORDER BY SUM(wait_time_ms) DESC
