df12195db7bfabbbd6a641aa479a16a179f5c364
Eleonora Grigoryan <eleonora.grigoryan@outlook.com>
2018-10-01 13:26:44 +0400

    Update Connected_Apps.sql

--- Audit/Connected_Apps.sql
+++ Audit/Connected_Apps.sql
@@ -6,8 +6,8 @@ select dm_dec.[client_net_address],
 dm_des.[program_name] ,
 dm_des.[host_name] ,
 dm_des.[login_name] ,
-count(dm_dec.session_id) as [connection_count],
-count(dm_des.session_id) as [session_count]
+count(dm_dec.connection_id) as connection_count,
+count(dm_dec.session_id) as [session_count]
 from sys.dm_exec_sessions as dm_des
 inner join sys.dm_exec_connections as dm_dec
 on dm_des.session_id = dm_dec.session_id
