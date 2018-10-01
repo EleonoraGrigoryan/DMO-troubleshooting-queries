0805c611b3b14df95ea1057c335c7fa108fad25d
Eleonora Grigoryan <eleonora.grigoryan@outlook.com>
2018-10-01 13:07:52 +0400

    Update Running_Applications.sql

--- Audit/Running_Applications.sql
+++ Audit/Running_Applications.sql
@@ -1,12 +1,13 @@
 --TSQL
 
---Identify currently running application names, IP address, host name, login name, and connection count
+--Identify currently running application names, IP address, host name, login name, and connection, session count
 
 select dm_dec.[client_net_address],
 dm_des.[program_name] ,
 dm_des.[host_name] ,
 dm_des.[login_name] ,
-count(dm_dec.session_id) as connection_count
+count(dm_dec.session_id) as [connection_count],
+count(dm_des.session_id) as [session_count]
 from sys.dm_exec_sessions as dm_des
 inner join sys.dm_exec_connections as dm_dec
 on dm_des.session_id = dm_dec.session_id
