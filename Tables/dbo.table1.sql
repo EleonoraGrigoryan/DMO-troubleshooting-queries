6833b021be8467a2b832f023c51a7aed5d651afe
EleonoraGrigoryan <eleonora.grigoryan@outlook.com>
2019-06-14 11:13:26 +0400

    mydb

--- Tables/dbo.table1.sql
+++ Tables/dbo.table1.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[table1] (
   [id] [int] NULL,
   [name] [varchar](50) NULL,
-  [age] [varchar](50) NULL
+  [age] [varchar](50) NULL,
+  [gender] [varchar](50) NULL
 )
 ON [SECONDARY]
 GO
